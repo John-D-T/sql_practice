e8712ab700144704483d2153e8e8b0c415f290f4
John T <john.david.tringham@gmail.com>
2023-12-25 09:34:58 +0800

    Add: hackerank problem - top competitors (wip)

---
+++ problems/hackerrank/top_competitors.sql
@@ -0,0 +1,10 @@
+-- join the hacker table to the the challenges table, then the difficulty table (we just want the difficulty column from these joins)
+    -- then using the challenge id, join this to the submission table
+
+-- obtain hacker id, hacker name
+-- filter on those who've obtained a full score for more than one challenge
+-- order by number of full scores, then by hacker id
+
+WITH cte AS (
+
+)
