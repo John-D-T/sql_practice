4b4092e65f518a7a6fb51d1af730320880a865ff
John T <john.david.tringham@gmail.com>
2023-12-25 11:29:40 +0800

    Add: hackerank problem - top competitors (wip)

--- problems/hackerrank/top_competitors.sql
+++ problems/hackerrank/top_competitors.sql
@@ -5,6 +5,26 @@
 -- filter on those who've obtained a full score for more than one challenge
 -- order by number of full scores, then by hacker id
 
-WITH cte AS (
+-- CTE doesn't work on MySql, only MSSQL
 
+
+-- WIP
+WITH cte AS (
+    SELECT h.hacker_id, h.name, d.difficulty_level, d.score, c.challenge_id
+    FROM Challenges c
+    LEFT JOIN Hackers h
+    ON c.hacker_id = h.hacker_id
+    LEFT JOIN Difficulty d
+    ON c.difficulty_level = d.difficulty_level
+),
+cte_2 AS (
+SELECT s.hacker_id, c.name, s.score, COUNT(name) OVER (PARTITION BY name)
+FROM Submissions s
+LEFT JOIN cte c
+ON s.challenge_id = c.challenge_id
+WHERE s.score = (c.difficulty_level * 20)
 )
+
+SELECT * FROM
+cte_2
+WHERE COUNT(DISTINCT name) > 2;
