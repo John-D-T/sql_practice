a2fc0b67a395050456d196bd26019694722aec52
John T <john.david.tringham@gmail.com>
2023-12-26 21:31:09 +0800

    Add: hackerank problem - challenges (wip)

---
+++ problems/hackerrank/challenges.sql
@@ -0,0 +1,16 @@
+/*
+Enter your query here.
+*/
+-- okay so we want hacker_id, name, and total number of challenges they've created
+
+-- sort by total number of challenges - desc, THEN hacker_id
+
+-- filter out rows which share the total number of challenges AND have a count less than the max number of challenges
+
+-- look to create 3 columns:
+    -- count() - total no of challenges
+    -- count() of the count() - might need a group by for this
+    -- max()
+
+
+-- WIP
