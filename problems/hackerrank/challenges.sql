3f26e70d9a36a7478abc61827e9f4669af3fb1b2
John T <john.david.tringham@gmail.com>
2023-12-26 23:11:08 +0800

    Add: hackerank problem - challenges (solved)

--- problems/hackerrank/challenges.sql
+++ problems/hackerrank/challenges.sql
@@ -1,16 +1,27 @@
-/*
-Enter your query here.
-*/
--- okay so we want hacker_id, name, and total number of challenges they've created
 
--- sort by total number of challenges - desc, THEN hacker_id
+-- we want hacker_id, name, and total_number of challengers
 
--- filter out rows which share the total number of challenges AND have a count less than the max number of challenges
+-- create a CTE with several key variables
+    -- COUNT
+    -- COUNT of the COUNT
+    -- MAX of the count
+-- use the GROUP BY to help with these windowing functions
 
--- look to create 3 columns:
-    -- count() - total no of challenges
-    -- count() of the count() - might need a group by for this
-    -- max()
+-- then select from the cte where the COUNT of the COUNT is 1, or the count = the max of the count
+
+WITH cte AS (
+SELECT h.hacker_id, h.name,
+       COUNT(*) AS challenge_count,
+       COUNT(*) OVER (PARTITION BY COUNT(*)) AS number_of_challenge_counts,
+       RANK() OVER (ORDER BY COUNT(*) DESC) AS challenge_rank
+FROM Challenges c
+LEFT JOIN Hackers h ON c.hacker_id = h.hacker_id
+GROUP BY h.hacker_id, h.name
+)
 
 
--- WIP
+SELECT ct.hacker_id, ct.name, ct.challenge_count
+FROM cte ct
+WHERE ct.number_of_challenge_counts = 1
+OR    ct.challenge_rank = 1
+ORDER BY ct.challenge_count DESC, ct.hacker_id ASC;
